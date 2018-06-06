769a4b4be567929e62b3091e8ade3e49bfd609a5
Oskar Goliński <oskargolinski@MacBook-Pro-Oskar.local>
2018-06-06 16:35:46 +0200

    little update

--- game.js
+++ game.js
@@ -3,13 +3,53 @@ const cardsColor = ["red", "red", "green", "green", "blue", "blue", "brown", "br
 let cards = document.querySelectorAll("div");
 cards = [...cards];
 
+const startTime = new Date().getTime();
 
-const init = () => {
+let activeCard = "";
+const activeCards = [];
+
+const gamePairs = cards.length/2;
+let gameResult = 0;
+
+const clickCard = function() {
+  activeCard = this;
+  activeCard.classList.remove("hidden");
+
+  if(activeCards.length === 0) {
+    activeCards[0] = activeCard;
+    return;
+  }
+  else {
+    cards.forEach((card) => {
+      card.removeEventListener("click", clickCard);
+    })
+    activeCards[1] = activeCard;
+    setTimeout(function() {
+      
+    
+    if (activeCards[0].className === activeCards[1].className) {
+      activeCards.forEach((card) => card.classList.add("off"))
+    }
+    else {
+      activeCards.forEach((card) => card.classList.add("hidden"))
+    }
+    }, 1000)
+  }
+};
+
+const init = function() {
   cards.forEach((card) => {
     const position = Math.floor(Math.random() * cardsColor.length);
     card.classList.add(cardsColor[position]);
     cardsColor.splice(position, 1)
   })
+
+  setTimeout(() => {
+    cards.forEach((card) => {
+      card.classList.add("hidden")
+      card.addEventListener("click", clickCard)
+    })
+  }, 2000)
 }
 
 init()
